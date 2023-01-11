4e78c7204122e5152c2c6c76601764b24aa1d068
Alexandr <sashakaratsiupa1995@gmail.com>
2023-01-11 22:22:55 +0200

    First Task Well Done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery=document.querySelector(".gallery");
+
+const listImg= galleryItems.map((element)=>`<div class="gallery__item">
+    <a class="gallery__link" href="${element.original}">
+        <img
+            class="gallery__image"
+            src="${element.preview}"
+            data-source="${element.original}"
+            alt="${element.description}"
+        />
+    </a>
+    </div>`)
+    .join('');
+gallery.innerHTML=listImg;
+
+let instance;
+
+gallery.addEventListener("click",(event)=>{
+    event.preventDefault();
+    const original =event.target.dataset.source;
+    // Бібліотека basiclightbox
+    instance = basicLightbox.create(`
+    <img src="${original}" width="800" height="600">`)
+    instance.show()
+    // Бібліотека basiclightbox
+});
+
+document.addEventListener("keydown", (e)=>{
+    console.log(e.key)
+    if(e.key==="Escape"){
+        // Бібліотека basiclightbox
+        instance.close();
+        // Бібліотека basiclightbox
+    }
+});
+
 console.log(galleryItems);
